066795e47e6e82c03d4ec6037cf8b64e1825c763
Juan Yasig <jcyasig2@espe.edu.ec>
2022-05-16 00:36:54 -0500

    vamos

---
+++ U1Lab2.py
@@ -0,0 +1,13 @@
+def suma():
+    a=int(input("Ingrese un numero: "))
+    b=int(input("Ingrese un numero anterior:"))
+    if (a>b):
+        total=a+b
+        print("El numero actual es: ", a)
+        print("El numero anterior es: ", b)
+        print("La suma de los dos numeros es: ",total)
+    else:
+        print("El numero ", b, " no es anterior a ",a)
+
+suma=suma()
+print(suma)
